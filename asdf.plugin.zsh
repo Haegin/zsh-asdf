725900308a9870e085db82604d2722a41201a69a
Harry Mills <harry@haeg.in>
2018-04-12 15:08:05 -0400

    Add asdf plugin with completion support

---
+++ asdf.plugin.zsh
@@ -0,0 +1,10 @@
+# Find where asdf should be installed.
+ASDF_DIR="${ASDF_DIR:-$HOME/.asdf}"
+
+# Load asdf, if found.
+if [ -f ${ASDF_DIR}/asdf.sh ]; then
+  . ${ASDF_DIR}/asdf.sh
+fi
+
+# Load completions
+. ${ASDF_DIR}/completions/asdf.bash
