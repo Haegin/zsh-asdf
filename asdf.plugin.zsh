2d0646530c008abcf27999503b1c8f625549f7c5
Harry Porter-Mills <harry@portermills.net>
2023-03-15 23:15:15 -0400

    Add support for Mac

--- asdf.plugin.zsh
+++ asdf.plugin.zsh
@@ -1,10 +1,15 @@
 # Find where asdf should be installed.
-ASDF_DIR="${ASDF_DIR:-$HOME/.asdf}"
+if [ "$(uname -s)" = "Darwin" ]; then
+  location="${HOMEBREW_PREFIX:-$(brew --prefix)}/opt/asdf"
+  . "${location}/libexec/asdf.sh"
+else
+  ASDF_DIR="${ASDF_DIR:-$HOME/.asdf}"
 
-# Load asdf, if found.
-if [ -f ${ASDF_DIR}/asdf.sh ]; then
-  . ${ASDF_DIR}/asdf.sh
+  # Load asdf, if found.
+  if [ -f ${ASDF_DIR}/asdf.sh ]; then
+    . ${ASDF_DIR}/asdf.sh
+
+    # Load completions
+    . ${ASDF_DIR}/completions/asdf.bash
+  fi
 fi
-
-# Load completions
-. ${ASDF_DIR}/completions/asdf.bash
